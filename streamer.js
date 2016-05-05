736f12cabb1485dbeffc1cb9c73e0b64aaf76f68
Mads Nedergaard <nedergaardmads@gmail.com>
2016-05-06 00:10:03 +0200

    Moved functionality from streamer into server.js, but kept functionality for quicker debugging

--- streamer.js
+++ streamer.js
@@ -2,14 +2,15 @@ var fs = require('fs');
 var wav = require('wav');
 var dgram = require('dgram');
 var chalk = require('chalk');
+var Stream = require('stream');  
 var Speaker = require('speaker');
 var udp = require('datagram-stream');
+var toArray = require('stream-to-array');
 
 var server = dgram.createSocket('udp4');
-
 var settings = {
   UDPPort: '3444',
-  partnerIP: '192.168.1.46'
+  partneIP: '192.168.1.46'
 };
 
 var stream = udp({
@@ -23,29 +24,38 @@ var stream = udp({
 
 
 
-console.log( // Welcome Message
-  '\n' +
-  chalk.red(',d88b.d88b,                      ,d88b.d88b,   ') + '\n' +
-  chalk.red('88888888888       STREAMER       88888888888   ') + '\n' +
-  chalk.red('`Y8888888Y´                      `Y8888888Y´   ') + '\n' +
-  chalk.red('  `Y888Y´                          `Y888Y´     ') + '\n' +
-  chalk.red('    `Y´                              `Y´       ') + '\n');
-console.log('  ' + chalk.underline('Status:'));
-
-// TODO: Make selection of file to stream dynamic!
-console.log(chalk.yellow('•')+' Fetching recording');
 var recording = fs.createReadStream('recordings/example.wav');
 var reader = new wav.Reader();
+var bufferArray = [];
 
 // Read wav to buffer
 reader.on('format', function(format){
   console.log(`${chalk.yellow('•')} Streaming to partner...`);
-  reader.pipe(new Speaker(format));
-  reader.pipe(stream);
+  reader.on('readable', () => {
+    var totalBuf = reader.read();
+    console.log(totalBuf);
+    var index = 0;
+    for (var i = 0; i < Buffer.byteLength(totalBuf); i = i+1024) {
+      if(totalBuf == null) { break; }
+      bufferArray[index] = totalBuf.slice(i, i+1024);
+//      sendStream(totalBuf.slice(i, i+1024));
+      stream.write(totalBuf.slice(i, i+1024));
+      index++;
+    }
+      });
 
+    function sendStream(buffer) {
+      setTimeout(function(){
+        server.send(buffer, settings.UDPPort, settings.partnerIP, function(){
+        });
+      }, 100);
+    }
+
+
+//  reader.on('end')
   // Start stream (should it run every 100ms or can it handle the entire chunk at once?)
-//  server.send(audioBuffer, settings.UDPPort, settings.partnerIP, function(){
-  //  server.close();
+//  server.send(Buffer.from('test'), settings.UDPPort, settings.partnerIP, function(){
+//    server.close();
 //  });
 
   // Tell Photon that the message is over, so we can set unreadMessage = false;
@@ -53,4 +63,9 @@ reader.on('format', function(format){
   // Give feedback to user
   console.log(chalk.green('•')+' Message sent');
 });
+
+//stream.pipe(process.stdout);
+//pipe whatever is received on stdin over udp
+//process.stdin.pipe(stream);
+
 recording.pipe(reader);
